c02f123b0ed6aa262e826d86d5fac7caa9f91202
Thambi <thambi@Piraviens-MacBook-Pro.local>
2025-02-11 15:25:20 +0000

    added dealCard and shuffleIntoNumberOrder methods

--- src/main/java/org/example/CardGame.java
+++ src/main/java/org/example/CardGame.java
@@ -10,28 +10,49 @@ public class CardGame {
             Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13")
     );
 
-    ArrayList<Card> deck = new ArrayList<Card>();
+    ArrayList<Card> deckOfCards = new ArrayList<Card>();
 
     public ArrayList<Card> getDeck() {
         for (String number : numbers) {
             Card spadeCard = new Card(Integer.parseInt(number),"♠");
-            deck.add(spadeCard);
+            deckOfCards.add(spadeCard);
             Card clubCard = new Card(Integer.parseInt(number),"♣");
-            deck.add(clubCard);
+            deckOfCards.add(clubCard);
             Card heartCard = new Card(Integer.parseInt(number),"♥");
-            deck.add(heartCard);
+            deckOfCards.add(heartCard);
             Card diamondCard = new Card(Integer.parseInt(number),"♦");
-            deck.add(diamondCard);
+            deckOfCards.add(diamondCard);
         }
-        return deck;
+        return deckOfCards;
     }
-    public ArrayList<String> describedDeck = new ArrayList<String>();
-    public ArrayList<String> to_String(){
 
-        for (Card card : deck){
+    public ArrayList<String> to_String(){
+        ArrayList<String> describedDeck = new ArrayList<String>();
+        for (Card card : deckOfCards){
             describedDeck.add(card.name);
         }
         System.out.println(describedDeck);
+        System.out.println(describedDeck.size());
     return describedDeck;
+    }
+
+    public Card dealCard() {
+        return deckOfCards.getFirst();
+    }
+
+    public void sortDeckInNumberOrder() {
+        ArrayList<Card> ascendingCards = new ArrayList<Card>();
+        for (int i = 0; i < deckOfCards.size(); i++ ){
+            for (int k = 0; k < deckOfCards.size(); k++){
+                if(deckOfCards.get(i).value <= deckOfCards.get(k).value){
+                    Card hold = deckOfCards.get(i);
+                    deckOfCards.set(i, deckOfCards.get(k));
+                    deckOfCards.set(k, hold);
+                }
+            }
+
+        }
+        System.out.println(deckOfCards);
+        System.out.println(deckOfCards.size());;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,6 +11,13 @@ package org.example;
 //        ● Has a String symbol (2,3,4,5,6,7,8,9,10,J,Q,K,A)
 //● Has an int value (2,3,4,5,6,7,8,9,10,11,12,13,14)
 //● Has a toString method that describes the class
+
+//Stage 2
+//Let's get some core functionality to our CardGame by implementing the following methods:
+//● Card dealCard() - Takes the card from the top of the deck and returns it.
+//● ArrayList<Card> sortDeckInNumberOrder() - Sorts the deck in number order (e.g. 2222333344445555 etc) and stores the new shuffled deck back into the deckOfCards attribute.
+//● ArrayList<Card> sortDeckIntoSuits() - Sorts the deck into suits (2,3,4,5,6,7,8,9,10,J,Q,K,A of hearts, then 2,3,4,5,6,7,8,9,10,J,Q,K,A of clubs etc.) and stores the new shuffled deck back into the deckOfCards attribute.
+//● ArrayList<Card> shuffleDeck() - Shuffles the deck into a random order and stores the new shuffled deck back into the deckOfCards attribute.
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -19,5 +26,8 @@ public class Main {
         CardGame game = new CardGame();
         game.getDeck();
         game.to_String();
-        }
+        game.sortDeckInNumberOrder();
+        game.to_String();
+
     }
+}
